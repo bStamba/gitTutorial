cd093e78916eacbaabfdadfe758e39a723cda4a3
Brandon Stamba <brandonstamba@gmail.com>
2023-07-12 13:28:52 -0500

    Hello World Version 1

---
+++ src/HelloWorldPackage/HelloWorld.java
@@ -0,0 +1,8 @@
+package HelloWorldPackage;
+
+//Method that returns the string "Hello World!" when called.
+public class HelloWorld {
+	public String getMessage() {
+		return "Hello World!";
+		}
+}
---
+++ src/HelloWorldPackage/main.java
@@ -0,0 +1,9 @@
+package HelloWorldPackage;
+public class main {
+
+	public static void main(String[] args) {
+		HelloWorld hello = new HelloWorld();
+		System.out.println(hello.getMessage());
+		System.out.println("Version 1!");
+	}
+}
